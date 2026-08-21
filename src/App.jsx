a840afd32cd74ab00c70c1bc4e6b93102e0c6491
Nozinn <bozorovanozina@gmail.com>
2026-08-21 15:37:37 +0500

    add about

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Header from './components/Header/Header';
 import Hero from './components/Hero/Hero';
 import Foodlist from './components/Foodlist/Foodlist';
+import About from './components/About/About'
 
 export default function App() {
   return (
@@ -8,6 +9,7 @@ export default function App() {
       <Header />
       <Hero />
       <Foodlist/>
+      <About />
     </div>
   );
 }
